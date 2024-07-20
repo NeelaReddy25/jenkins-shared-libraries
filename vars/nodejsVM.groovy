78baae74a050594833e0aa8b387b231cce0309b4
NeelaReddy <neelareddy.i25@gmail.com>
2024-07-20 18:39:31 +0530

    good effects

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -15,6 +15,7 @@ def call(Map configMap){
             account_id = pipelineGlobals.account_id()
             component = configMap.get("component")
             project = configMap.get("project")
+            def releaseExists = ""
         }
         stages {
             stage('read the version'){
@@ -45,6 +46,7 @@ def call(Map configMap){
                     """
                 }
             }
+
             stage('Docker build'){
                 steps{
 
@@ -55,16 +57,64 @@ def call(Map configMap){
                     """
                 }
             }
+
             stage('Deploy'){
                 steps{
-                    sh """
-                        aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                        cd helm
-                        sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                        helm upgrade ${component} -n ${project} .
-                    """
+                    script{
+                        releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
+                        if(releaseExists.isEmpty()){
+                            echo "${component} not installed yet, first time installation"
+                            sh """
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm install ${component} -n ${project} .
+                            """
+                        }
+                        else{
+                            echo "${component}exists, running upgrade"
+                            sh """
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm upgrade ${component} -n ${project} .
+                            """
+                        }
+                    } 
                 }
             }
+
+            stage('verify Deployment'){
+                steps{
+                    script{
+                        rollbackStatus = sh(script: "kubectl rollout status deployment/frontend -n ${project} --timeout=1m || true", returnStdout: true).trim()
+                        if(rollbackStatus.contains('successfully rolled out')){
+                            echo "Deployment is sucessfull"
+                        }
+                        else{
+                            echo "Deployment is failed, performing rollback"
+                            if(releaseExists.isEmpty()){
+                                error "Deployment failed, not able to rollback, since it is first time deployment"
+                            }
+                            else{
+                                sh """
+                                    aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                    helm rollback frontend -n ${project} 0
+                                    sleep 60
+                                """
+                                rollbackStatus = sh(script: "kubectl rollout status deployment/frontend -n expense --timeout=2m || true", returnStdout: true).trim()
+                                if(rollbackStatus.contains("successfully rollout")){
+                                    echo "Deployment is failed, Rollback is successfully"
+                                }
+                                else{
+                                    error "Deployment is failed, Rollback is failed"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             
             /* stage('Nexus Artifact Upload'){
                 steps{
@@ -75,12 +125,12 @@ def call(Map configMap){
                             nexusUrl: "${nexusUrl}",
                             groupId: 'com.expense',
                             version: "${appVersion}",
-                            repository: "backend",
+                            repository: "frontend",
                             credentialsId: 'nexus-auth',
                             artifacts: [
-                                [artifactId: "backend" ,
+                                [artifactId: "frontend" ,
                                 classifier: '',
-                                file: "backend-" + "${appVersion}" + '.zip',
+                                file: "frontend-" + "${appVersion}" + '.zip',
                                 type: 'zip']
                             ]
                         )
